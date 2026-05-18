63632717da5b2653159b2dfb1f75c3f37f976148
subratyadav99 <subrat060606@gmail.com>
2026-05-18 23:53:48 +0530

    Completed Kubernetes and Terraform integration

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+
+  required_providers {
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
+
+  }
+
+}
+
+provider "kubernetes" {
+
+  config_path = "~/.kube/config"
+
+}
+
+resource "kubernetes_namespace" "devopsflow" {
+
+  metadata {
+    name = "devopsflow-namespace"
+  }
+
+}
